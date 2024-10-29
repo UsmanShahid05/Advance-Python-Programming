85c715b4648f61eb17a54326e2f0fc26a55c2494
Usman Shahid <140746601+UsmanShahid05@users.noreply.github.com>
2024-10-29 14:10:28 +0500

    Intro to basic python code.

---
+++ intro_to_python.py
@@ -0,0 +1,46 @@
+""""
+Intro to python. Python, very popular and most favorite lauguage that is build for developer to work fast, 
+and resolve their problem easily. Let's begin our journey with python. We will inialize our journey with
+Basic code and then we'll proceed to advance as we move on. """
+
+""" The first code for the python developer is non other than printing Hello World!. Let's try this."""
+
+print("Hello World!") # Explination => To print the output on screen we always use "print()" fucntion, function are basially particular line of code that perform predefined step of instructions.
+# To print value in function, we use parenthesis " ( ) " and if the value is string ( "charactors") then we put these into quotation, either single or double.
+
+# Let's write a simple code for basic calculation between two values.
+
+# Assigning value to variable. At this time think variable as boxes that store certain things. We use '=' to assign value to variable.
+
+first_value= 4 # Declaring first variable
+second_value=8 # Declaring second variable.
+
+# Calculating sum, subtraction, multiplication, division and exponentional value of these two variables.
+
+# Addition of values.
+# Declaring third variable to store the value.
+third_variable=first_value+second_value 
+print(third_variable)
+# We can add values even not using third variable let's try it.
+print(first_value+second_value)
+
+
+# Subtracting value.
+subtration=first_value-second_value
+print(subtration)
+
+# Multiplication
+multiplication=first_value*second_value # "estaric '*' is use for multiplication."
+print(multiplication)
+
+#Division
+division=first_value/second_value # "forward slash '/' is used for division."
+print(division)
+
+#Exponentional operation.
+power_factor=first_value ** second_value # "exponentional operator is represented as '*' and between the values and operator we can also put space."
+print(power_factor)
+
+
+# As all these calculation needs operators like plus, minus, estaric, division etc. All these operators are know as "Operators or Variable Operator."
+# We will learn more about these in our comming lectures.
